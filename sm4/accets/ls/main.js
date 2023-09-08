4dcb92bfdcbd5c7682152ca8e57686aa251fd7de
ilnazSS <samigullinilnaz52@gmail.com>
2023-09-08 10:28:30 +0300

    first commit

---
+++ sm4/accets/ls/main.js
@@ -0,0 +1,19 @@
+let logo = console.log(document.querySelector('.logo'))
+console.log(logo)
+
+let link = console.log(document.querySelectorAll('.link'))
+console.log(link)
+
+let email_js = document.getElementById('email')
+console.log(email_js)
+
+logo.style.background = 'red'
+logo.style.padding = '10px'
+logo.style.borderRadius = '10px'
+
+let textarea = document.getElementById('.textarea')
+textarea.textContent
+
+
+let forbox = document.querySelector('.forbox')
+forbox.innerHTML = box
